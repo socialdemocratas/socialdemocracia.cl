3a9f39a1a9cd027c17dcd4ef70b7c2828bc298eb
= <r.gnzlz.cstll@gmail.com>
2022-09-24 06:50:50 -0300

    first commit

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,18 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+
+/// <reference types="@sveltejs/adapter-cloudflare" />
+
 declare namespace App {
 	// interface Locals {}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
 	interface Platform {
-	    env: {
+	    env?: {
 	        //COUNTER: DurableObjectNamespace;
-			ARTICLES: '6b873574420e41d685161ef9f53c045e'
+			ARTICLES: { binding: "ARTICLES", id: "6b873574420e41d685161ef9f53c045e" }
 	    };
 	    context: {
 	        waitUntil(promise: Promise<any>): void;
--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,9 +1,9 @@
 import { error } from '@sveltejs/kit';
  
 /** @type {import('./$types').RequestHandler} */
-export function GET({ platform }) {
+export async function GET({ platform }) {
   //console.log('args', args, Object.keys(args), args.platform.env);
-  const result = platform.env.ARTICLES.list({"prefix": 't'});
+  const result = await platform.env.ARTICLES.list({"prefix": 't'});
   //return new Response("holi")
   return result;
 }
