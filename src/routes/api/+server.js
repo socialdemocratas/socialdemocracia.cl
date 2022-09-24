650c395e5b8e43797b96014a5d36a9cf2e37fdde
= <r.gnzlz.cstll@gmail.com>
2022-09-24 06:54:36 -0300

    first commit

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,9 +1,9 @@
 import { error } from '@sveltejs/kit';
  
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ platform }) {
+export async function GET({ request, platform }) {
   //console.log('args', args, Object.keys(args), args.platform.env);
   const result = await platform.env.ARTICLES.list({"prefix": 't'});
   //return new Response("holi")
-  return result;
+  return new Response(result);
 }
