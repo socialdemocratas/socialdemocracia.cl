ede97622f45f721c01ee1a33d3ef88e43d205125
= <r.gnzlz.cstll@gmail.com>
2022-09-24 06:56:05 -0300

    first commit

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -5,5 +5,5 @@ export async function GET({ request, platform }) {
   //console.log('args', args, Object.keys(args), args.platform.env);
   const result = await platform.env.ARTICLES.list({"prefix": 't'});
   //return new Response("holi")
-  return new Response(result);
+  return new Response(JSON.stringify(result));
 }
