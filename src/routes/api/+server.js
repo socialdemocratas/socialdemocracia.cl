36b6ab850bc880c6bc63f542c6443554d3be87ae
= <r.gnzlz.cstll@gmail.com>
2022-09-24 06:42:37 -0300

    first commit

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -5,5 +5,5 @@ export function GET({ platform }) {
   //console.log('args', args, Object.keys(args), args.platform.env);
   const result = platform.env.ARTICLES.list({"prefix": 't'});
   //return new Response("holi")
-  return new Response(JSON.stringify(result));
+  return result;
 }
