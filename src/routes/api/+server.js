b7ee4e60d123702f852d35e508e5cd94c97fb5e0
= <r.gnzlz.cstll@gmail.com>
2022-09-24 06:39:19 -0300

    first commit

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,8 +1,9 @@
 import { error } from '@sveltejs/kit';
  
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ request, platform }) {
-    const result = platform.env.ARTICLES.list({"prefix": 't'});
- 
+export function GET({ platform }) {
+  //console.log('args', args, Object.keys(args), args.platform.env);
+  const result = platform.env.ARTICLES.list({"prefix": 't'});
+  //return new Response("holi")
   return new Response(JSON.stringify(result));
 }
